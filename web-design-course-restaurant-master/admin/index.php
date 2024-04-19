97eef9fc407e672612d5792a153eba3e8cc75993
KajalLimb <159853699+KajalLimb@users.noreply.github.com>
2024-04-19 14:18:19 -0400

    video 1
    Corrected

--- web-design-course-restaurant-master/admin/index.php
+++ web-design-course-restaurant-master/admin/index.php
@@ -3,24 +3,25 @@
         <div class= "wrapper"> 
         <h1>Dashboard</h1>
         <div class="col-4 text-center">
-     <h1>5</h1> <br />
+     <h1>5</h1> 
+     <br />
       Categories
-        </div> </div>  </div>
+        </div> 
 
         <div class="col-4 text-center">
      <h1>5</h1> <br />
       Categories
-        </div> </div>  </div>
+        </div> 
 
         <div class="col-4 text-center">
      <h1>5</h1> <br />
       Categories
-        </div> </div>  </div>
+        </div> 
 
         <div class="col-4 text-center">
      <h1>5</h1> <br />
       Categories
-        </div> </div>  </div>
+        </div> 
 
         <div class="clearfix"></div>
 
