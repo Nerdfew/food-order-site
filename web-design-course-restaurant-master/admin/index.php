0e29208bcfad734b3cd6392650a4176287fb1ca5
KajalLimb <159853699+KajalLimb@users.noreply.github.com>
2024-05-01 00:37:51 -0500

    video 12
    Completed

--- web-design-course-restaurant-master/admin/index.php
+++ web-design-course-restaurant-master/admin/index.php
@@ -13,26 +13,58 @@
              unset($_SESSION['login']);
          }
       ?>
-<br>
+<br><br>
         <div class="col-4 text-center">
-     <h1>5</h1> 
+
+        <?php 
+        $sql = "SELECT * FROM category";
+
+        $res = mysqli_query($conn,$sql);
+        $count = mysqli_num_rows($res);
+       
+        ?>
+
+        <br>
+     <h1><?php echo $count ;?></h1> 
      <br />
       Categories
         </div> 
 
         <div class="col-4 text-center">
-     <h1>5</h1> <br />
-      Categories
+        <?php 
+        $sql2 = "SELECT * FROM food";
+
+        $res2 = mysqli_query($conn,$sql2);
+        $count2 = mysqli_num_rows($res2);
+       
+        ?>
+     <h1><?php echo $count2 ;?></h1> <br />
+      Foods
         </div> 
 
         <div class="col-4 text-center">
-     <h1>5</h1> <br />
-      Categories
+        <?php 
+        $sql3 = "SELECT * FROM orders";
+
+        $res3 = mysqli_query($conn,$sql3);
+        $count3 = mysqli_num_rows($res3);
+       
+        ?>
+     <h1><?php echo $count3 ;?></h1> <br />
+     Total Orders
         </div> 
 
         <div class="col-4 text-center">
-     <h1>5</h1> <br />
-      Categories
+          <?php 
+          $sql4 = "SELECT SUM(orders_total) AS Total FROM orders WHERE status= 'Delivered'";
+          $res4 = mysqli_query($conn,$sql4);
+          $row4 = mysqli_fetch_assoc($res4);
+
+          $total_revenue = $row4['Total'];
+          
+          ?>
+     <h1>$<?php echo $total_revenue ;?></h1> <br />
+      Revenue Generated
         </div> 
 
         <div class="clearfix"></div>
